0c841057e378b211fbdb384355c051c7844d5969
Raphael Voellmy <r.voellmy@gmail.com>
2022-07-21 20:37:43 +0400

    disable pricing page

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -8,16 +8,17 @@ interface Props {
 }
 
 export default function PricingPage({ products }: Props) {
-  return <Pricing products={products} />;
+  return null;
+  // return <Pricing products={products} />;
 }
 
-export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
-  const products = await getActiveProductsWithPrices();
+// export async function getStaticProps(): Promise<GetStaticPropsResult<Props>> {
+//   // const products = await getActiveProductsWithPrices();
 
-  return {
-    props: {
-      products,
-    },
-    revalidate: 60,
-  };
-}
+//   return {
+//     props: {
+//       // products,
+//     },
+//     revalidate: 60,
+//   };
+// }
