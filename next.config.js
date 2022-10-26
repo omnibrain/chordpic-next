ff22179a43e73da028ac4c1c13f015dbfb783f78
Raphael Voellmy <r.voellmy@gmail.com>
2022-10-26 23:36:18 +0200

    downgrade eslint rules

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const { withSentryConfig } = require("@sentry/nextjs");
 const moduleExports = {
   reactStrictMode: true,
   swcMinify: true,
+  sentry: {
+    hideSourceMaps: false,
+  },
 };
 
 const sentryWebpackPluginOptions = {
