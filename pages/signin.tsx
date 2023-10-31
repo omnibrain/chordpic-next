9a20695bfa1f199f564a0f04a98bfdb8fa72fc31
Raphael Voellmy <r.voellmy@gmail.com>
2023-10-31 21:51:37 +0100

    add warning to the signup form

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -5,6 +5,8 @@ import { useRouter } from "next/router";
 import { FormEvent, useEffect, useState } from "react";
 
 import {
+  Alert,
+  AlertIcon,
   Box,
   Button,
   Center,
@@ -98,6 +100,16 @@ const SignIn = () => {
   if (!user)
     return (
       <AuthBox title={t("Sign in to Chordpic")}>
+        <Alert status="warning">
+          <AlertIcon />
+          <T>
+            Due to a technical problem with our authentication provider log-ins
+            and sign-ups are currently not possible. We are in contact with the
+            Supabase support to resolve the problem as soon as possible. We
+            apologize for the inconvenience.
+          </T>
+        </Alert>
+
         {message.content && (
           <Text color="red.500" fontSize="sm">
             <T>{message.content}</T>
